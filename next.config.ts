cd4e7cebe07b62f3074abed04b55f6d20dc1ae83
Suhaas V <suhaasvijay7@gmail.com>
2025-05-26 19:58:52 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/specs",
+  assetPrefix: "/specs",
+  trailingSlash: false,
 };
 
 export default nextConfig;
